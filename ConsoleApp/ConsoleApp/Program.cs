c89605f409e301569ffffdb7d245c21f5afed16d
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-02-27 19:06:02 +0300

    example

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("line 1");
+            Console.WriteLine("line 2");
+            Console.WriteLine("line 3");
+            Console.WriteLine("line 4");
+
+            Console.ReadLine();
+            Console.WriteLine("end");
+        }
+    }
+}
